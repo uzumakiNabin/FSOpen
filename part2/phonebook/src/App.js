768c24b05c7663dd9783ebed48733e438995a8fe
uzumakiNabin <nabin.kaucha@gmail.com>
2023-02-21 14:05:11 +0545

    part 2 ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import PersonsList from "./components/PersonsList";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data))
+      .catch((err) => console.log("error fetching", err));
+  }, []);
 
   const getFilteredPersons = () => {
     return persons.filter((person) => person.name.toLowerCase().includes(filterText));
