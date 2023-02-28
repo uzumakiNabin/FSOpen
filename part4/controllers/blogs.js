465208a41b527df9444eae9204232de3f9feffe9
uzumakiNabin <nabin.kaucha@gmail.com>
2023-02-28 21:15:02 +0545

    fixed routes

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,21 +1,33 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+    const newBlog = await blog.save();
+    response.status(201).json(newBlog);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-  blog
-    .save()
-    .then((newBlog) => {
-      response.status(201).json(newBlog);
-    })
-    .catch((error) => next(error));
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findByIdAndRemove(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;
